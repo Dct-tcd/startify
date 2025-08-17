dfe0c1710e1d68b488dced7f685e104ec0e8e1dc
rishi22-hub <rishichaitanyatiwari22@gmail.com>
2025-08-17 19:42:08 +0530

    UI imrpovements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,20 @@ export default function Navbar({ onToggleSidebar }) {
           <button onClick={onToggleSidebar} className="md:hidden btn btn-ghost text-gray-100/90">
             <Menu size={20} />
           </button>
-          <div className="font-semibold tracking-tight">
+          {/* <div className="font-semibold tracking-tight">
             <span className="text-gray-100">DEVRI-AI</span>
-          </div>
+          </div> */}
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="240" height="60" viewBox="0 0 240 60">
+  <circle cx="25" cy="25" r="22" fill="#0284C7"/>
+  <text x="25" y="32" font-size="22" text-anchor="middle" fill="white">🧠</text>
+
+
+  <text x="70" y="38" font-size="24" font-family="Arial, sans-serif" font-weight="bold" fill="#f1ececff">
+    CodeMentor <tspan fill="#0284C7">AI</tspan>
+  </text>
+</svg>
+
         </div>
 
         <button className="btn btn-primary">Login</button>
